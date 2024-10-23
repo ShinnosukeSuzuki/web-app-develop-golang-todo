c4731a2bad85b7f20392c780bf36d00b207b7983
shinnosuke-suzuki <shinnosuke.suzuki@geniee.co.jp>
2024-10-23 11:00:30 +0900

    commit sec-056
    ポート番号を変更できるようにする

--- main_test.go
+++ main_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"net"
 	"net/http"
 	"testing"
 
@@ -11,15 +12,22 @@ import (
 )
 
 func TestRun(t *testing.T) {
+	//  portに0を指定することで、システムが自動的に利用可能なポートを選択
+	l, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		t.Fatalf("failed to listen port %v", err)
+	}
 	// テスト用のコンテキストを作成
 	ctx, cancel := context.WithCancel(context.Background())
 	eg, ctx := errgroup.WithContext(ctx)
 	eg.Go(func() error {
-		return run(ctx)
+		return run(ctx, l)
 	})
 
 	in := "message"
-	rep, err := http.Get("http://localhost:18080/" + in)
+	url := fmt.Sprintf("http://%s/%s", l.Addr().String(), in)
+	t.Logf("try request to %q", url)
+	rep, err := http.Get(url)
 	if err != nil {
 		t.Errorf("failed to get: %v", err)
 	}
