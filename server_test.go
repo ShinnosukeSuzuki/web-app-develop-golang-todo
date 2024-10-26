f9459399858cf46bb761855effc636107beb89bf
shinnosuke-suzuki <shinnosuke.suzuki@geniee.co.jp>
2024-10-26 17:32:22 +0900

    commit sec-062
    「Server」構造体を定義する

--- server_test.go
+++ server_test.go
@@ -11,42 +11,42 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
-func TestRun(t *testing.T) {
-	t.Skip("リファクタリング中")
-
-	//  portに0を指定することで、システムが自動的に利用可能なポートを選択
+func TestServer_Run(t *testing.T) {
 	l, err := net.Listen("tcp", "localhost:0")
 	if err != nil {
 		t.Fatalf("failed to listen port %v", err)
 	}
-	// テスト用のコンテキストを作成
 	ctx, cancel := context.WithCancel(context.Background())
 	eg, ctx := errgroup.WithContext(ctx)
-	eg.Go(func() error {
-		return run(ctx)
+	mux := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 	})
 
+	eg.Go(func() error {
+		s := NewServer(l, mux)
+		return s.Run(ctx)
+	})
 	in := "message"
 	url := fmt.Sprintf("http://%s/%s", l.Addr().String(), in)
+	// どんなポート番号でリッスンしているのか確認
 	t.Logf("try request to %q", url)
-	rep, err := http.Get(url)
+	rsp, err := http.Get(url)
 	if err != nil {
-		t.Errorf("failed to get: %v", err)
+		t.Errorf("failed to get: %+v", err)
 	}
-	defer rep.Body.Close()
-	got, err := io.ReadAll(rep.Body)
+	defer rsp.Body.Close()
+	got, err := io.ReadAll(rsp.Body)
 	if err != nil {
 		t.Fatalf("failed to read body: %v", err)
 	}
-
-	// HTTPサーバーの戻り値を検証
-	want := fmt.Sprintf("%s\n", in)
+	// HTTPサーバーの戻り値を検証する
+	want := fmt.Sprintf("Hello, %s!", in)
 	if string(got) != want {
 		t.Errorf("want %q, but got %q", want, got)
 	}
-
-	// run関数に終了通知を送る
+	// run関数に終了通知を送信する。
 	cancel()
+	// run関数の戻り値を検証する
 	if err := eg.Wait(); err != nil {
 		t.Fatal(err)
 	}
